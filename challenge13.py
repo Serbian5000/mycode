10c129de5f90055eb9900f0533c459bebbcec7e8
Serbian5000 <ianserbu@gmail.com>
2024-02-06 19:21:41 +0000

    join and split

---
+++ challenge13.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Prompt user for name
+print("what is your name?")
+name = input('')
+# pause and collect input (wait for ENTER)
+# Promt user for day of the week
+print("what day of the week is it?")
+day = input('')
+# pause and collect input (wait for ENTER)
+# Print the sentence:
+print(f'Hello, {name}! Happy {day}!')

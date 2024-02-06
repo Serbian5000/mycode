8afd1b55e8d568f2d1e931f90d584a77fd3512e1
Serbian5000 <ianserbu@gmail.com>
2024-02-06 21:21:13 +0000

    mixed lists

---
+++ mix-list/mix-list-challange01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# given the list iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"] display only the IP addresses on the screen.
+
+ip_list = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print( ip_list[3] + " " + ip_list[4])
